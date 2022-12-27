5a5b4d12b6934419ac4bb2c9b3fb30bb16746fdb
catresium <oktaykarakas71@gmail.com>
2022-12-27 22:56:51 +0300

    update auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,7 @@ const register = async (req, res) => {
     { userId: user._id, name: user.name, email: user.email },
     process.env.JWT_SECRET,
     {
-      expiresIn: '1h',
+      expiresIn: '30d',
     },
   )
   res.json({
@@ -46,7 +46,7 @@ const login = async (req, res) => {
   const token = jwt.sign(
     { userId: user._id, name: user.name, email: user.email },
     process.env.JWT_SECRET,
-    { expiresIn: '1h' },
+    { expiresIn: '30d' },
   )
 
   res.json({
